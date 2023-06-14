6b3948aeee1a941a47d3907c627780d335e99019
BuilderDemo7 <136541955+BuilderDemo7@users.noreply.github.com>
2023-06-14 00:21:35 -0300

    a Lua script to use the module
    a Lua script to use the module,
    this can be an example for your next mod menu!
    LoL

--- useprocess.lua
+++ useprocess.lua
@@ -1,11 +1,9 @@
 local process = require "process"
 
-local dpldata,ID = process.FindWindow("notepad++")
---local written = process.WriteProcessMemory(ID,10824828,"Hacked!")
+local data,ID = process.FindWindow("notepad++")
+process.WriteProcessMemory(ID,10824828,"Hacked!")
 local memory = process.ReadProcessMemory(ID,10824828,121)
 
-print("Process -: "..tostring(dpldata)..", "..ID)
+print("Process -: "..tostring(data)..", "..ID)
 print("Process -: "..memory..", size of "..#memory.." bytes")
---print("Process written memory -; "..tostring(written))
-print("Done")
---print(0x23E82498)
+print("Done.")
